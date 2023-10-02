eaedc93e91dd83bfb18775bcfec7d1f95d8ce8f3
Hiren Makwana <hirenm563@gmail.com>
2023-10-02 16:06:32 +0530

    #1 Base Created

---
+++ android/app/src/main/kotlin/com/example/calandermm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calandermm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Presentation/Scheduling_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Schedulingpage extends StatefulWidget {
+  const Schedulingpage({super.key});
+
+  @override
+  State<Schedulingpage> createState() => _SchedulingpageState();
+}
+
+class _SchedulingpageState extends State<Schedulingpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
